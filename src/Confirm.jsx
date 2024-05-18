417c02f552ab7867ef5bded8d43bee0b22cb9124
Alfredo <fredoaranda@gmail.com>
2024-05-17 19:41:33 -0600

    changes

--- src/Confirm.jsx
+++ src/Confirm.jsx
@@ -532,16 +532,15 @@ function Confirm() {
           </div>
         </div>
 
-         {/* <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.min.js"></script>
         <script src="js/modernizr-2.6.2.min.js"></script>
-        <script src="js/jquery.easing.1.3.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.waypoints.min.js"></script>
         <script src="js/sticky-kit.min.js"></script>
         <script src="js/isotope.js"></script>
         <script src="js/jquery.magnific-popup.min.js"></script>
         <script src="js/owl.carousel.min.js"></script>
-        <script src="js/main.js"></script>  */}
+        <script src="js/main.js"></script>  
       </div>
     </>
   )
